384fe4035d70aa89247104d317417d0cd4ea8f7a
Lewis Davidson <lewisdavidson8@gmail.com>
2024-03-10 02:51:54 +0000

    Adjust TMDB api object to be a singleton.
    Change env constants to be upper snake case for constant identifier.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
-const apiKey = String.fromEnvironment('API_KEY');
+// ignore_for_file: constant_identifier_names
+
+const TMDB_API_KEY = String.fromEnvironment('API_KEY');
 
 const bearerToken = String.fromEnvironment('BEARER_TOKEN');
 
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
--- lib/repositories/apis/tmdb_api.dart
+++ lib/repositories/apis/tmdb_api.dart
@@ -1,13 +1,35 @@
-import 'package:http/http.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:movieradar/constants.dart';
 
 class TMDBApi {
-  const TMDBApi(this._client);
-  final Client _client;
+  TMDBApi._internal();
 
-  static const _apiBaseUrl = tmdbBaseUrl;
+  // Define a singleton instance for the TMDBApi
+  factory TMDBApi() => instance;
+  static final TMDBApi instance = TMDBApi._internal();
 
-  Future<Response> get(String path) async {
-    return _client.get(Uri.parse('$_apiBaseUrl$path'));
+  static const _apiBaseUrl = TMDB_BASE_URL;
+  static const _apiKey = TMDB_API_KEY;
+  final http.Client _httpClient = http.Client();
+
+  // Defines a default get method that always passes in the api key using .replace
+  Future<dynamic> get(String endpoint,
+      {Map<String, dynamic>? queryParameters}) async {
+    Uri uri = Uri.parse('$_apiBaseUrl/$endpoint');
+    if (queryParameters != null) {
+      uri = uri.replace(queryParameters: queryParameters);
+    }
+
+    uri = uri.replace(queryParameters: {'api_key': _apiKey});
+
+    final response = await _httpClient.get(uri);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load data from TMDB API');
+    }
   }
 }
