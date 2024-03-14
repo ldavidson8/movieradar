7ccc44509a9057b5a7af06f1ea56e81fc0cdf78f
Lewis Davidson <lewisdavidson8@gmail.com>
2024-03-14 16:18:29 +0000

    Changed reference to Theme cubit file in app file.

--- lib/ui/widgets/app.dart
+++ lib/ui/widgets/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:movieradar/cubits/theme_cubit.dart';
+import 'package:movieradar/blocs/theme_cubit.dart';
 import 'package:movieradar/themes.dart';
 import 'package:movieradar/ui/screens/error.dart';
 
